334f12a5f100b2eb1cbbf96cc294b0c2a0792258
Rohit <rohityadav2604@gmail.com>
2021-07-03 01:06:21 +0530

    memodone

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let generateBoard = document.querySelector(".Generate");
 let Board = document.querySelector(".Board");
 let Path = document.querySelector(".min-path");
+let memopath = document.querySelector(".memo");
+let stop = document.querySelector(".stop");
 
-
+stop.addEventListener("click" , function()
+{
+    location.reload();
+})
 generateBoard.addEventListener("click" , CreateBoard);
 Path.addEventListener("click" , GeneratePath);
+memopath.addEventListener("click" , GeneratePathMemo);
 
 let BoardArray = [];
 function CreateBoard(e)
@@ -52,9 +58,9 @@ function CreateBoard(e)
 }
 
 
-async function allpath(i , j , sum , maxsum , minPath , count)
+async function allpath(i , j , sum , maxsum )
 {
-    console.log(count);
+    
      if(i>=20)
      {
          return;
@@ -63,39 +69,95 @@ async function allpath(i , j , sum , maxsum , minPath , count)
      {
          return;
      }
+     
      if(i==19 && j==19)
      {
              return;
      }
     
-     let currentBox = document.querySelector(`[rowid="${i}"][colid="${j}"]`);
-     currentBox.classList.add("active");
-    //  await new Promise((res) =>{
-    //     setTimeout(()=>{
-    //          res();
-    //     } , 100)
-    // });
-    
 
-    // await new Promise((res) =>{
-    //     setTimeout(()=>{
-    //         allpath(i+1 , j+1 , sum+BoardArray[i][j].value , maxsum , Path);
-    //         allpath(i+1 , j+1 , sum+BoardArray[i][j].value , maxsum , Path);
-    //         currentBox.classList.remove("active");
-    //          res();
-    //     } , 100)
-    // });
-     allpath(i , j+1 , sum+BoardArray[i][j].value , maxsum , Path , count+1);
-     allpath(i+1 , j , sum+BoardArray[i][j].value , maxsum , Path , count+1);
-    // currentBox.classList.add("active");
+     let currentBox = document.querySelector(`[rowid="${i}"][colid="${j}"]`);
+    //  if(currentBox.classList.contains("visited"))
+    //  {
+    //      return;
+    //  }
+      currentBox.classList.add("active");
+     
+     await new Promise((res) =>{
+        setTimeout(()=>{
+             res();
+        } , 100)
+    });
+    
+     await allpath(i , j+1 , sum+BoardArray[i][j].value , maxsum);
+    await allpath(i+1 , j , sum+BoardArray[i][j].value , maxsum );
+    //currentBox.classList.add("visited");
+    currentBox.classList.remove("active");
 
     
 }
-function GeneratePath()
+
+
+
+async function allpathmemo(i , j , sum , maxsum)
 {
+    console.log("allpathmemo");
+
+    if(i>=20)
+     {
+         return;
+     }
+     if(j>=20)
+     {
+         return;
+     }
+     
+     if(i==19 && j==19)
+     {
+             return;
+     }
+    
+
+     let currentBox = document.querySelector(`[rowid="${i}"][colid="${j}"]`);
+     if(currentBox.classList.contains("visited"))
+     {
+         return;
+     }
+      currentBox.classList.add("active");
+     
+     await new Promise((res) =>{
+        setTimeout(()=>{
+             res();
+        } , 100)
+    });
+    
+     await allpathmemo(i , j+1 , sum+BoardArray[i][j].value , maxsum);
+    await allpathmemo(i+1 , j , sum+BoardArray[i][j].value , maxsum );
+    currentBox.classList.add("visited");
+    currentBox.classList.remove("active");
+
+
+
+    
+}
+
+function GeneratePathMemo()
+{
+    console.log("generatepathmemo")
     let sum = 0;
     let maxsum = -1;
-    let minPath = [];
-    allpath(0 , 0 , sum , maxsum , minPath , 0);
+   
+    allpathmemo(0 , 0 , sum , maxsum);
+
+}
+
+
+function GeneratePath()
+{
+    console.log("generatepath");
+    let sum = 0;
+    let maxsum = -1;
+   
+    allpath(0 , 0 , sum , maxsum );
 
 }
